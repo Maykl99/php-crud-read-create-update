79a64d773ca47e8f570092fb0f232a0e6b4bcf6e
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-09-29 16:29:00 +0200

    php

--- partials_php/delete/server.php
+++ partials_php/delete/server.php
@@ -4,17 +4,18 @@ include __DIR__.'/../database.php';
 
 if(empty($_POST['id'])){
     die('nessun id');
+}else{
+    $id=$_POST['id'];
+    $sql = "DELETE FROM stanze WHERE id=$id";
+    $result = $conn->query($sql);
+
+    if($result):
+        echo 'eliminato';
+    else: echo 'non posso eliminare questa riga';
+    endif;
+    
 }
 
-$id=$_POST['id'];
-
-$sql = "DELETE FROM stanze WHERE id=$id";
-$result = $conn->query($sql);
-
-if($result):
-    echo 'eliminato';
-else: echo 'non posso eliminare questa riga';
-endif;
 #header('location: index.php');
 
 
