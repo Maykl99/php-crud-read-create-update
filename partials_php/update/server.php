7fca3d3c41cc0de427bedb46ab44ec4b8fe1646a
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-10-02 00:07:55 +0200

    bug update risolto

--- partials_php/update/server.php
+++ partials_php/update/server.php
@@ -7,8 +7,6 @@ if(empty($_POST['id']) && empty($_POST['beds'])){
     die('nessun id');
 }
 
-$id=$_POST['id'];
-#getId($conn,'stanze',$id);
 
 $sql = "UPDATE stanze SET room_number=?  WHERE id=?"; #update_at= NOW()
 $stmt=$conn->prepare($sql); // preparo la mia sql
@@ -17,13 +15,14 @@ $stmt->bind_param('ii',$room_number,$id); // lego la query sostituendo il segnap
 $id=$_POST['id'];
 $room_number=$_POST['room_number'];
 
-var_dump($stmt);
+#var_dump($stmt);
 
 /* if($stmt && $stmt->affected_rows > 0){
     header("location: $basepath/index.php?id=$id&update=1&delete=0");
-}elseif($stmt && $stmt->affected_rows == 0){
-    header("location: $basepath/index.php?id=$id&update=0&delete=0");
 } */
+if($stmt && $stmt->affected_rows == 0){
+    header("location: $basepath/index.php?id=$id&update=1&delete=0");
+}
 $stmt->execute();
 
 $stmt->close();
